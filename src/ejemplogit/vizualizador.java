d55794c011bb6fc8ec49f3ec943fa3c6a7fdb072
Alejandro Miranda <alejandromsz@hotmail.com>
2020-08-29 14:43:49 -0500

    Se agrego el comentario descriptivo y se cambio el autor

--- src/ejemplogit/vizualizador.java
+++ src/ejemplogit/vizualizador.java
@@ -1,14 +1,15 @@
+package ejemplogit;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ejemplogit;
 
 /**
- *
- * @author alejandro
+ * Esta es una clase para vizualizar resultador
+ * @author Alejandro Miranda Sanchez <alejandromsz@hotmail.com>
  */
-public class visualizador {
+public class vizualizador {
     
 }
