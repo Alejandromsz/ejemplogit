d86e05e009585315b9a79b710db6b89e8c119c58
Alejandro Miranda <alejandromsz@hotmail.com>
2020-08-29 15:03:55 -0500

    Se agrego la clase Estudiante

---
+++ src/ejemplogit/Estudiante.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogit;
+
+/**
+ *
+ * @author Alejandro Miranda Sanchez <alejandromsz@hotmail.com>
+ */
+public class Estudiante {
+    
+}
